c53ebb143759ecd323aa1fde8c9cc7706a8b0a43
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-12 12:57:11 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке 
+// от M до N с помощью рекурсии.
+
+string RecNatur(int m, int n)
+{
+    if (m >= n-2) return $"{m}";
+    return $"{m} " + RecNatur(m + 2, n);
+}
+
+int InputInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ReductionToEven(int number)
+{
+    if (number % 2 != 0) return number +=1;
+    else return number +=2;
+}
+
+int m = InputInt("Input minimum");
+int n = InputInt("Input maximum");
+m = ReductionToEven(m);
+System.Console.Write(RecNatur(m, n));
