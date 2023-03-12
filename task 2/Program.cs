70c3eef3a6cb081df98e0d7c184e96d8dcf71751
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-12 13:13:51 +0300

    task 2 solved

---
+++ task 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int InputInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int RecSum(int m, int n, int sum)
+{
+    if (m == n - 1) return sum;
+    m +=1;
+    return sum += m + RecSum(m, n, sum);
+}
+
+
+
+
+int m = InputInt("Input minimum");
+int n = InputInt("Input maximum");
+int sum = 0;
+int totalSum = RecSum(m, n, sum);
+System.Console.WriteLine($"Sum of all natural numbers between {m} and {n} is equal {totalSum}");
